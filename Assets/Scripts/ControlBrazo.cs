19e01eea7fcb0758ce1ce935d292cc6603ba482e
Javier Contreras <javierin_kl@hotmail.com>
2019-02-12 22:56:19 -0700

    Initial commit

---
+++ Assets/Scripts/ControlBrazo.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBrazo : MonoBehaviour
+{
+    public Transform BaseGrua;
+    public Transform Grua;
+    public float FuerzaBrazo;
+    public float Cuerpo;
+    private float Angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Angle = Grua.localRotation.eulerAngles.x;
+        BaseGrua.parent = transform;
+        Grua.parent = transform;
+    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Sirve");
+            transform.eulerAngles += -Vector3.up * Cuerpo * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log(Angle);
+            transform.eulerAngles += Vector3.up * Cuerpo * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+     
+            if(Angle == -100)
+            {
+
+            }
+            else
+            {
+                Grua.transform.eulerAngles += Vector3.right * FuerzaBrazo * Time.deltaTime;
+            }
+     
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log(transform.localRotation.eulerAngles);
+            if (Angle == -40)
+            {
+
+            }
+            else
+            {
+                Grua.transform.eulerAngles += -Vector3.right * FuerzaBrazo * Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GruaController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GruaController : MonoBehaviour
+{
+    public Transform Base;
+    public Transform Cuerpo;
+    public Transform BaseGrua;
+    public Transform Grua;
+    public Transform Grua2;
+    public Transform Grua3;
+    public Transform Grua4;
+    private Rigidbody rb;
+    public float Velocidad;
+    public AudioSource Andando;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Base.parent = transform;
+        Cuerpo.parent = transform;
+        BaseGrua.parent = transform;
+        Grua.parent = transform;
+        Grua2.parent = transform;
+        Grua3.parent = transform;
+        Grua4.parent = transform;
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Sirve");
+            rb.AddForce(-transform.up* Velocidad);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Sirve");
+            rb.AddForce(transform.up * Velocidad);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Sirve");
+            transform.eulerAngles += -Vector3.up * 40 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Sirve");
+            transform.eulerAngles += Vector3.up * 40 * Time.deltaTime;
+        }
+    }
+}
